03ea0f8d692b27a1a891f7e1def2b41329defddb
Ibukun Didier <adjitchedidier1@gmail.com>
2019-12-05 11:57:54 -0400

    Adicionando paramêtros de Heroku

---
+++ backend/src/models/Product.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+//Schema
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    categoria: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true
+    },
+    active: {
+        type: Boolean,
+        require: true,
+        defaut: 1
+    },
+    images: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+}
+// ,{
+//     timestamps:true
+// }
+);
+
+// File.virtual('url').get(function(){
+//     return
+// })
+
+module.exports =mongoose.model('Product', ProductSchema);
---
+++ backend/src/routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const routes = express.Router();
+const multer = require('multer')
+const uploadConfig = require('./config/imageUploads')
+const upload = multer(uploadConfig)
+
+const UserController = require('./controllers/UserController');
+routes.get('/user', UserController.index);
+routes.get('/user/:id', UserController.show);
+routes.post('/user', UserController.store);
+routes.put('/user/:id', UserController.update);
+routes.post('/user/:id', UserController.destroy);
+
+routes.post('/user_cart/:id/add_cart/:product', UserController.addCart);
+routes.post('/user_cart/:id/rem_cart/:product', UserController.remCart);
+routes.post('/finalizar_compra/:id', UserController.finalizarCompra);
+
+const ProductController = require('./controllers/ProductController');
+routes.get('/product', ProductController.index);
+routes.get('/product/:id', ProductController.show);
+routes.post('/product',upload.single('images'),ProductController.store);
+routes.post('/product_atualizar/:id', ProductController.update);
+routes.post('/product/:id', ProductController.destroy);
+
+routes.get('/', (req,res)=>{
+    return res.send("Bem-vindo no projeto NetClothes");
+})
+
+module.exports = routes;
